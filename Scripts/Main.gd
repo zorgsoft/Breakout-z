9fc7f79133685c05124b1cb9cbce6f6b6d0052c7
Sergey Agarkov <zorgsoft@gmail.com>
2017-11-15 18:47:20 +0300

    Change mouse control, added touch control
    Redesigned mouse control and added touch control for mobile devices.
    Added horizontal movement restrictions for the platform.

--- Scripts/Main.gd
+++ Scripts/Main.gd
@@ -2,10 +2,11 @@ extends Node2D
 
 var score = 0 setget set_score
 
+onready var paddle = get_node('Paddle')
+
 # Load level data on Main ready
 # -----------------------------------------------------------------------------
 func _ready():
-	var paddle = get_node('Paddle')
 	var ball_start_x = get_viewport().get_rect().size.x / 2
 	var ball_start_y = paddle.get_global_pos().y - (paddle.get_node('Sprite').get_item_rect().size.height / 2)
 	
@@ -14,8 +15,12 @@ func _ready():
 	# Create new ball
 	Levels.ball_add(Vector2(ball_start_x, ball_start_y), Vector2(200, -200))
 	
+	get_viewport().connect("size_changed", self, "window_resize")
+	
 	set_process_input(true)
 
+# On input
+# -----------------------------------------------------------------------------
 func _input(event):
 	if event.is_action_pressed("ui_fullscreen"):
 		OS.set_window_fullscreen(!OS.is_window_fullscreen())
@@ -23,7 +28,15 @@ func _input(event):
 	if event.is_action_pressed("ui_cancel"):
 		get_tree().quit()
 
+# Set new score
+# -----------------------------------------------------------------------------
 func set_score(score_add):
 	var score_label = get_node('UIContainer/ScoreLabel')
 	score += score_add
 	score_label.set_text('Score: ' + str(score))
+
+# On window resize
+# -----------------------------------------------------------------------------
+func window_resize():
+	# paddle.update_limits()
+	pass
--- Scripts/Paddle.gd
+++ Scripts/Paddle.gd
@@ -1,14 +1,43 @@
 extends KinematicBody2D
 
 export (int) var MOVE_SPEED = 2
+
 var position = Vector2()
 
+export (int) var move_limit_left = 128
+export (int) var move_limit_right = -128
+var calculated_move_limit_left
+var calculated_move_limit_right
+
+var input_pos = Vector2(0, 0)
+
+
 func _ready():
+	update_limits()
+	set_process_input(true)
 	set_fixed_process(true)
+
+func _input(event):
+	if event.type == InputEvent.MOUSE_MOTION or event.type == InputEvent.SCREEN_TOUCH:
+		input_pos = event.pos
 
 func _fixed_process(delta):
 	var paddle_x = get_pos().x
-	var mouse_x = get_viewport().get_mouse_pos().x
+	var mouse_x = input_pos.x
+
 	position.x += (mouse_x - paddle_x) * MOVE_SPEED * delta
 	position.y = get_pos().y
+	
+	if position.x <= calculated_move_limit_left:
+		position.x = calculated_move_limit_left
+	if position.x >= calculated_move_limit_right:
+		position.x = calculated_move_limit_right
+	
 	set_pos(position)
+
+func update_limits():
+	var sprite_width = get_node("Sprite").get_texture().get_width()
+	
+	calculated_move_limit_left = move_limit_left + sprite_width
+	calculated_move_limit_right = get_viewport_rect().size.width - sprite_width + move_limit_right
+	print(calculated_move_limit_right)
